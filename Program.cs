93a5c033393ba74b8975cc9be7b734521f719eb1
Alex Tavares <cicero1000alex@gmail.com>
2026-02-15 00:34:43 -0300

    feat: Troca de Valores

--- Program.cs
+++ Program.cs
@@ -25,3 +25,49 @@ int idadeUsuario = Convert.ToInt32(Console.ReadLine());
 int diasIdade = idadeUsuario * 365;
 Console.WriteLine("Os dias são: " + diasIdade);
 #endregion
+
+#region 3.Troca de Valores
+Console.Write("Informe um valor A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Informe um valor B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nValor Original do A: " + a + "\nValor Original do B: " + b);
+
+int valor1 = a;
+int valor2 = b;
+a = valor2;
+b = valor1;
+
+Console.WriteLine("\nOs valores trocados são: \nA - " + a + "\nB - " + b);
+#endregion
+
+//Nível 2: Estruturas Condicionais (if, else if, else)
+#region 1. Verificador de Maioridade
+#endregion
+
+#region 2. Classificador de Número
+#endregion
+
+#region 3. Validação de Login Simples
+#endregion
+
+////Nível 3: Estruturas de Repetição (Loops)
+#region 1. Contagem Regressiva
+#endregion
+
+#region 2. Soma de Ímpares (Loop while)
+#endregion
+
+#region 3. Tabuada Personalizada
+#endregion
+
+//Nível 4: Coleções (Arrays e Listas)
+#region 1. Inversão de Array
+#endregion
+
+#region 2. Busca e Contagem em Lista (foreach)
+#endregion
+
+#region 3. Gerenciador de Tarefas Simples (CRUD Básico em Memória)
+#endregion
